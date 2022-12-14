500d23c16658295086e00e5e2a4047eda321bde4
Hestirix <dmhango95@gmail.com>
2022-12-14 14:45:03 -0800

    how

--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -1,23 +1,11 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * main - check the code
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * main - check the code
+ * Return: Always 0.
+ */
 int main(void)
-
 {
-
 	    jack_bauer();
-
-	        return (0);
-
+	    return (0);
 }
