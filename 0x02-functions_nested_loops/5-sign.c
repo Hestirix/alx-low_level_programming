62fb19d7d6a8e8966d8c4edb98fdb36fff1ea295
Hestirix <dmhango95@gmail.com>
2022-12-14 13:45:44 -0800

    almost done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * printing signs
+ * @n: number checked
+ * Return: 1 for positive, -1 for negative or zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
