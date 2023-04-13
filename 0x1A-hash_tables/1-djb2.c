9fc9f08477aa72f96902c0147c78ff9113f1683c
Hestirix <dmhango95@gmail.com>
2023-04-12 18:40:59 -0700

    completions hash

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - Hash function implementing the djb2 algorithm
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
  * @str: The string to hash.
  *
  * Return: The calculated hash.
@@ -9,10 +9,11 @@
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-		int c;
+	int c;
 
-		hash = 5381;
+	hash = 5381;
 	while ((c = *str++))
-		hash = ((hash << 5) + hash) +c; /* hash * 33 +c */
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
 	return (hash);
+}
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - Deletes a hash table
- * @ht: A pointer to a hash table
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
  */
 void hash_table_delete(hash_table_t *ht)
 {
@@ -12,14 +12,14 @@ void hash_table_delete(hash_table_t *ht)
 
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] 1= NULL)
+		if (ht->array[i] != NULL)
 		{
 			node = ht->array[i];
 			while (node != NULL)
 			{
 				tmp = node->next;
 				free(node->key);
-				free(node->value):
+				free(node->value);
 				free(node);
 				node = tmp;
 			}
